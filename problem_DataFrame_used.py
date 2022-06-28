2d205a535e2ee3c1458fb244362daa48fdf74f80
박수빈 <87634136+subinbing@users.noreply.github.com>
2022-06-29 01:13:53 +0900

    Add files via upload

---
+++ problem_DataFrame_used.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# data 생성
+data = {"A" : ["A0", "A1", "A2", "A3"],
+            "B" : ["B0", "B1", "B2", "B3"],
+            "C" : ["C0", "C1", "C2", "C3"],
+            "D" : ["D0", "D1", "D2", "D3"],
+            "F" : ["F0", "F1", "F2", "F3"],
+            "key" : ["K0", "K1", "K2", "K3"]
+            }
+
+# DataFrame 생성
+df = pd.DataFrame(data)
+
+# 이름으로 참조 -> loc 사용
+df = df.loc[range(0, 4), ["A", "B", "key", "C", "D"]]
+
+# df 출력
+print(df)
